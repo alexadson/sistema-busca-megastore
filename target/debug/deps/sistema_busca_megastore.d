b1e9e59a9b0273908de45f78fceaa3ecb99d4811
alex adson <alexadson111@gmai.com>
2025-05-06 17:03:52 -0300

    Primeiro commit

---
+++ target/debug/deps/sistema_busca_megastore.d
@@ -0,0 +1,5 @@
+C:\Users\gamer\sistema-busca-megastore\target\debug\deps\sistema_busca_megastore.exe: src\main.rs
+
+C:\Users\gamer\sistema-busca-megastore\target\debug\deps\sistema_busca_megastore.d: src\main.rs
+
+src\main.rs:
---
+++ target/debug/sistema-busca-megastore.d
@@ -0,0 +1,1 @@
+C:\Users\gamer\sistema-busca-megastore\target\debug\sistema-busca-megastore.exe: C:\Users\gamer\sistema-busca-megastore\src\main.rs
